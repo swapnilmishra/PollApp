deb3d1d8916adde9f1188748c77f693314f1fd5c
Swapnil <swapmis1084@gmail.com>
2020-02-23 08:49:48 +0100

    Create PollService to handle http requests

---
+++ src/services/PollService.js
@@ -0,0 +1,28 @@
+export const QUESTIONS_URL_KEY = "questions_url";
+
+export class PollService {
+  static questionsUrl = null;
+  static pollApiRoot = null;
+
+  static init = async pollApiRoot => {
+    this.pollApiRoot = pollApiRoot;
+    const response = await fetch(pollApiRoot);
+    const responseJSON = await response.json();
+    this.questionsUrl = `${pollApiRoot}${responseJSON[QUESTIONS_URL_KEY]}`;
+  };
+
+  static fetchPollQuestions = async () => {
+    const response = await fetch(this.questionsUrl);
+    const responseJSON = await response.json();
+    return responseJSON;
+  };
+
+  static postSelectedPollChoice = async pollChoice => {
+    const { url } = pollChoice;
+    const response = await fetch(`${this.pollApiRoot}${url}`, {
+      method: "POST"
+    });
+    const responseJSON = await response.json();
+    return responseJSON;
+  };
+}
