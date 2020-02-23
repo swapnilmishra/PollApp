af03251c08345992eb415cb3a6610363a2cdd153
Swapnil <swapmis1084@gmail.com>
2020-02-23 08:52:17 +0100

    Use PollService for various requests
    * Integrate fetching of poll questions
    * Integrate saving of poll choices

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import data from "./data";
 import { QuestionsList } from "./components/QuestionsList/QuestionsList";
 import { Poll } from "./components/Poll/Poll";
+import { PollService } from "./services/PollService";
 
 const PAGE_HOME = "questions_list";
 const PAGE_POLL = "poll";
+const POLL_API_HOST = "https://polls.apiblueprint.org";
 
-function App() {
+const App = () => {
   const [currentPage, setPage] = useState(PAGE_HOME);
   const [selectedQuestion, setSelectedQuestion] = useState(null);
+  const [pollQuestions, setPollQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchPollQuestions() {
+      await PollService.init(POLL_API_HOST);
+      const pollQuestions = await PollService.fetchPollQuestions();
+      setPollQuestions(pollQuestions);
+    }
+
+    fetchPollQuestions();
+  }, [currentPage]);
 
   switch (currentPage) {
     case PAGE_POLL:
       return (
-        <div className="App-header">
-          <Poll
-            question={selectedQuestion}
-            onVote={selectedChoice => console.log(selectedChoice)}
-          />
-        </div>
+        <Poll
+          question={selectedQuestion}
+          onVote={async selectedChoice => {
+            await PollService.postSelectedPollChoice(selectedChoice);
+            setPage(PAGE_HOME);
+          }}
+          onBack={() => setPage(PAGE_HOME)}
+        />
       );
     default:
       return (
-        <div className="App-header">
-          <QuestionsList
-            questions={data}
-            onQuestionSelection={question => {
-              setSelectedQuestion(question);
-              setPage(PAGE_POLL);
-            }}
-          />
-        </div>
+        <QuestionsList
+          questions={pollQuestions}
+          onQuestionSelection={question => {
+            setSelectedQuestion(question);
+            setPage(PAGE_POLL);
+          }}
+        />
       );
   }
-}
+};
 
-export default App;
+export default () => (
+  <>
+    <h1>Quizzify App</h1>
+    <App />
+  </>
+);
