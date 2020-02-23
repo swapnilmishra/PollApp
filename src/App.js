c4c90e5af68f9e5634da9347dc57dc244235b450
Swapnil <swapmis1084@gmail.com>
2020-02-23 09:58:17 +0100

    Refactor home page component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { QuestionsList } from "./components/QuestionsList/QuestionsList";
-import { Poll } from "./components/Poll/Poll";
-import { PollService } from "./services/PollService";
-import { CreatePoll } from "./components/CreatePoll/CreatePoll";
+import React, { useState } from "react";
 import { Button } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
-
-const PAGE_HOME = "questions_list";
-const PAGE_POLL = "poll";
-const POLL_API_HOST = "https://polls.apiblueprint.org";
-
-const ViewPolls = () => {
-  const [currentPage, setPage] = useState(PAGE_HOME);
-  const [selectedQuestion, setSelectedQuestion] = useState(null);
-  const [pollQuestions, setPollQuestions] = useState([]);
-
-  useEffect(() => {
-    async function fetchPollQuestions() {
-      await PollService.init(POLL_API_HOST);
-      const pollQuestions = await PollService.fetchPollQuestions();
-      setPollQuestions(pollQuestions);
-    }
-    fetchPollQuestions();
-  }, [currentPage]);
-
-  switch (currentPage) {
-    case PAGE_POLL:
-      return (
-        <Poll
-          question={selectedQuestion}
-          onVote={async selectedChoice => {
-            await PollService.postSelectedPollChoice(selectedChoice);
-            setPage(PAGE_HOME);
-          }}
-          onBack={() => setPage(PAGE_HOME)}
-        />
-      );
-
-    default:
-      return (
-        <QuestionsList
-          questions={pollQuestions}
-          onQuestionSelection={question => {
-            setSelectedQuestion(question);
-            setPage(PAGE_POLL);
-          }}
-        />
-      );
-  }
-};
+import "./App.css";
+import { PollService } from "./services/PollService";
+import { CreatePoll } from "./components/CreatePoll/CreatePoll";
+import { ViewPolls } from "./containers/PollHome";
 
 export default () => {
   const [isInCreateMode, setCreateMode] = useState(false);
---
+++ src/containers/PollHome.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { QuestionsList } from "../components/QuestionsList/QuestionsList";
+import { Poll } from "../components/Poll/Poll";
+import { PollService } from "../services/PollService";
+
+const PAGE_HOME = "questions_list";
+const PAGE_POLL = "poll";
+const POLL_API_HOST = "https://polls.apiblueprint.org";
+
+export const ViewPolls = () => {
+  const [currentPage, setPage] = useState(PAGE_HOME);
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+  const [pollQuestions, setPollQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchPollQuestions() {
+      await PollService.init(POLL_API_HOST);
+      const pollQuestions = await PollService.fetchPollQuestions();
+      setPollQuestions(pollQuestions);
+    }
+    fetchPollQuestions();
+  }, [currentPage]);
+
+  switch (currentPage) {
+    case PAGE_POLL:
+      return (
+        <Poll
+          question={selectedQuestion}
+          onVote={async selectedChoice => {
+            await PollService.postSelectedPollChoice(selectedChoice);
+            setPage(PAGE_HOME);
+          }}
+          onBack={() => setPage(PAGE_HOME)}
+        />
+      );
+
+    default:
+      return (
+        <QuestionsList
+          questions={pollQuestions}
+          onQuestionSelection={question => {
+            setSelectedQuestion(question);
+            setPage(PAGE_POLL);
+          }}
+        />
+      );
+  }
+};
